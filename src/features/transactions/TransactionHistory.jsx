db01ba65c285ccd7309ef23b073824dcf1dea592
elhusch <ellihunt@umich.edu>
2024-11-14 15:43:04 -0500

    final commit

---
+++ src/features/transactions/TransactionHistory.jsx
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux";
+import "./transactionHistory.scss";
+
+/** Displays a table row with transaction information  */
+const TransactionRow = ({ transaction: { type, amount, balance } }) => (
+  <tr>
+    <th scope="row">{type}</th>
+    <td>{amount.toFixed(2)}</td>
+    <td>{balance.toFixed(2)}</td>
+  </tr>
+);
+
+/** Displays a table of the user's transaction history. */
+export default function TransactionHistory() {
+  // TODO: Get the transaction history from the Redux store
+  // using the useSelector hook
+  const history = useSelector((state) => state.transactions.history);
+
+  console.log(history);
+  return (
+    <section className="transactions-history container">
+      <h2>Transaction History</h2>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Type</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* TODO
+          Map over the transactions in `history`
+          to render the appropriate `TransactionRow`s
+          */}
+          {history.map((hist, index) => {
+            return <TransactionRow key={index} transaction={hist} />;
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+}
---
+++ src/features/transactions/Transactions.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux"; //
+
+import { transfer, withdrawal, deposit } from "./transactionsSlice";
+import "./transactions.scss";
+
+/**
+ * Allows users to deposit to, withdraw from, and transfer money
+ * from their account.
+ */
+export default function Transactions() {
+  // TODO: Get the balance from the Redux store using the
+  // useSelector hook
+
+  const balance = useSelector((state) => state.transactions.balance); //
+  //issue I had with most of the code was needing to insert ".transactions"
+  const dispatch = useDispatch();
+
+  const [amountStr, setAmountStr] = useState("0.00");
+  const [recipient, setRecipient] = useState("");
+
+  /** Dispatches a transaction action based on the form
+   * submission. */
+  const onTransaction = (e) => {
+    e.preventDefault();
+
+    // This changes depending on which button the user clicked
+    //to submit the form.
+    // It will be either "deposit", "withdraw", or "transfer".
+    const action = e.nativeEvent.submitter.name;
+
+    const amount = +amountStr;
+
+    // TODO: Dispatch the appropriate transaction action
+    //based on `action`
+    if (action === "transfer") {
+      // The `transfer` action is dispatched with a payload
+      //containing
+      // the amount and the recipient.
+      dispatch(transfer({ amount, recipient }));
+    } else if (action === "withdraw") {
+      dispatch(withdrawal({ amount, recipient })); //
+    } else if (action === "deposit") {
+      dispatch(deposit({ amount, recipient })); //
+    }
+  };
+
+  return (
+    <section className="transactions container">
+      <h2>Transactions</h2>
+      <figure>
+        <figcaption>Current Balance &nbsp;</figcaption>
+        <strong>${balance.toFixed(2)}</strong>
+      </figure>
+      <form onSubmit={onTransaction}>
+        <div className="form-row">
+          <label>
+            Amount
+            <input
+              type="number"
+              inputMode="decimal"
+              min={0}
+              step="0.01"
+              value={amountStr}
+              onChange={(e) => setAmountStr(e.target.value)}
+            />
+          </label>
+          <div>
+            <button default name="deposit">
+              Deposit
+            </button>
+            <button name="withdraw">Withdraw</button>
+          </div>
+        </div>
+        <div className="form-row">
+          <label>
+            Transfer to
+            <input
+              placeholder="Recipient Name"
+              name="recipient"
+              value={recipient}
+              onChange={(e) => setRecipient(e.target.value)}
+            />
+          </label>
+          <button name="transfer">Transfer</button>
+        </div>
+      </form>
+    </section>
+  );
+}
